732a5d9484446c6e03b570810803015e4600841c
German Martinez <germaaan@gmail.com>
2016-03-28 01:58:58 +0200

    Añade login

--- Restaurantes/app_restaurantes/views.py
+++ Restaurantes/app_restaurantes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     context = {'mensaje': 'Hola mundo!'}
     return render(request, 'app_restaurantes/base.html', context)
+
+def login(request):
+    return HttpResponse("Usuario: " + request.POST.get('usuario') + " Password: " + request.POST.get('password'))
