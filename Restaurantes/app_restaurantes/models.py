e16ffac668000a922d3dfc1bb39216b922509843
German Martinez <germaaan@gmail.com>
2016-03-28 14:03:29 +0200

    Añade modelos

--- Restaurantes/app_restaurantes/models.py
+++ Restaurantes/app_restaurantes/models.py
@@ -1,5 +1,34 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Restaurante (models.Model):
+	nombre    = models.CharField(max_length=30)
+	direccion = models.CharField(max_length=60)
+	email     = models.EmailField()
+    slug      = models.SlugField()
+
+    def save(self, *args, **kwargs):
+		self.slug = slugify(self.nombre)
+		super(Restaurante, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.nombre
+
+class Plato (models.Model):
+	restaurante = models.ForeingKey(Restaurante, on_delete=models.CASCADE)
+	nombre      = models.CharField(max_lenght=30)
+	precio      = models.DecimalField(max_digits=3, decimal_places=1)
+	foto        = models.ImageField(upload_to='profile_images', blank=True)
+
+    def save(self, *args, **kwargs):
+		self.slug = slugify(self.nombre)
+		super(Plato, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.nombre
+
+    def barato(self):
+		if self.precio < 10:
+			return "barato"
+		else:
+			return "caro"
